940d8f429f62c43b94a5844683fc0a5c5bc25822
Matheus <85845749+rmathr@users.noreply.github.com>
2023-07-25 11:28:23 -0400

    add compression, helmet and express-rate-limit configuration

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const path = require('path');
+const compression = require('compression');
+const helmet = require('helmet');
+const RateLimit = require('express-rate-limit');
 
 mongoose.set('strictQuery', false);
 const mongoDB = process.env.MONGODB_URL;
@@ -13,19 +16,24 @@ async function main() {
 }
 
 const indexRouter = require('./routes/index');
-const catalogRouter = require('./routes/catalog'); //Import routes for "catalog" area of site
+const catalogRouter = require('./routes/catalog');
 
-// console.log(process.env.MONGODB_URL);
-// app.set('views', path.join(__dirname, 'views'));
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
+
+app.use(helmet());
+
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
-// app.use(express.static('public'));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// app.get('/', (req, res) => {
-//   res.render('index');
-// });
 app.use('/', indexRouter);
 app.use('/catalog', catalogRouter);
 
